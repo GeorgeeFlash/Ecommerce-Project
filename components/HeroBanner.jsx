67a55c17ecf2d2c9d158656b6d100c4188e3d5ec
GeorgeeFlash <georgeeflash@gmail.com>
2024-01-02 04:58:55 +0100

    HeroBanner component complete

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,45 @@
+"use client"
+
+import Link from 'next/link'
+import React from 'react'
+
+import { imageProps } from '@/sanity/lib/image'
+import Image from 'next/image';
+
+const HeroBanner = ({ heroBanner }) => {
+
+  const imgProps = imageProps(heroBanner.image);
+
+  console.log("ImageProps: ", imgProps)
+
+  return (
+    <div className='hero-banner-container'>
+      <div className="">
+        <p className="beats-solo">
+          {heroBanner.smallText}
+        </p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <Image 
+          {...imgProps}
+          style={{ width: '250', height: '250' }}
+          alt="headphones" 
+          className='hero-banner-image image-cover' 
+        />
+        <div className="">
+          <Link
+            href={`/product/${heroBanner.product}`}
+          >
+            <button type='button'>{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroBanner
--- sanity/lib/image.js
+++ sanity/lib/image.js
@@ -1,12 +1,6 @@
-import createImageUrlBuilder from '@sanity/image-url'
+import { useNextSanityImage } from 'next-sanity-image'
+import { client } from './client'
 
-import { dataset, projectId } from '../env'
-
-const imageBuilder = createImageUrlBuilder({
-  projectId: projectId || '',
-  dataset: dataset || '',
-})
-
-export const urlForImage = (source) => {
-  return imageBuilder?.image(source).auto('format').fit('max').url()
-}
+export const imageProps = (image) => {
+  return useNextSanityImage(client, image);
+} 
